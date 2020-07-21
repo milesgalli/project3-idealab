2af409f8a6ba4a8985bd75aab8eb0e1960cb96f5
mgalli <galli.miles@gmail.com>
2020-07-21 12:05:53 +0800

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,30 @@ import Navbar from "./componments/layouts/Navbar";
 import Landing from "./componments/layouts/Landing";
 import Register from "./componments/auth/Register";
 import Login from "./componments/auth/Login";
+import Alert from './componments/layouts/Alert'
 
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => (
-  <Router>
-  <Fragment>
-    <Navbar />
-    <Route exact path = "/" component = {Landing}/>
-    <section className = "container">
-      <Switch>
-        <Route exact path = "/register" component = {Register}></Route>
-        <Route exact path = "/login" component = {Login}></Route>
-      </Switch>
-    </section>
-  </Fragment>
-
-  </Router>
+  <Provider store = {store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Alert/>
+          <Switch>
+            <Route exact path="/register" component={Register}></Route>
+            <Route exact path="/login" component={Login}></Route>
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
--- client/src/componments/auth/Register.js
+++ client/src/componments/auth/Register.js
@@ -1,6 +1,11 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
+import PropTypes from "prop-types";
 
-export const Register = () => {
+export const Register = ({ setAlert, register }) => {
   const [formData, setFromData] = useState({
     name: "",
     email: "",
@@ -9,75 +14,81 @@ export const Register = () => {
   });
 
   const { name, email, password, password2 } = formData;
+
   const onChange = (e) =>
     setFromData({ ...formData, [e.target.name]: e.target.value });
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
+
     if (password !== password2) {
-      console.log("passwords do not match");
+      setAlert("Passwords do not match", "danger");
     } else {
-      console.log(formData);
+      register({name, email, password});
     }
   };
   return (
     <Fragment>
-      <section className="container">
-        <h1 className="large text-primary">Sign Up</h1>
-        <p className="lead">
-          <i className="fas fa-user"></i> Create Your Account
-        </p>
-        <form className="form" onSubmit={(e) => onSubmit(e)}>
-          <div className="form-group">
-            <input
-              type="text"
-              placeholder="Name"
-              name="name"
-              value={name}
-              onChange={(e) => onChange(e)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="email"
-              placeholder="Email Address"
-              name="email"
-              value={email}
-              onChange={(e) => onChange(e)}
-            />
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={(e) => onChange(e)}
+    
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+          />
 
-            <small className="form-text">
-              This site uses Gravatar so if you want a profile image, use a
-              Gravatar email
-            </small>
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              minLength="6"
-              value={password}
-              onChange={(e) => onChange(e)}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              name="password2"
-              minLength="6"
-              value={password2}
-              onChange={(e) => onChange(e)}
-            />
-          </div>
-          <input type="submit" className="btn btn-primary" value="Register" />
-        </form>
-        <p className="my-1">
-          Already have an account? <a href="login.html">Sign In</a>
-        </p>
-      </section>
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            minLength="6"
+            value={password2}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
     </Fragment>
   );
 };
-export default Register;
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
+
+};
+export default connect(null, { setAlert, register })(Register);
